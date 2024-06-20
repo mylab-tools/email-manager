30b518553714a4c08413a3faca4aa52e9daf2a81
Andrey O. <ozzy.ext@gmail.com>
2024-06-20 19:07:42 +0300

    Add sending configuration

---
+++ src/MyLab.EmailManager.Infrastructure/SendingEntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyLab.EmailManager.Domain.Entities;
+
+namespace MyLab.EmailManager.Infrastructure;
+
+public class SendingEntityTypeConfiguration : IEntityTypeConfiguration<Sending>
+{
+    public void Configure(EntityTypeBuilder<Sending> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
