ffb493522b04a5f47106fbaeeccabbbf2796c040
Andrey O. <ozzy.ext@gmail.com>
2024-06-28 20:04:27 +0300

    Create db context for migration

---
+++ src/Migrations/MigrationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLab.EmailManager.Infrastructure;
+
+namespace Migrations
+{
+    public class MigrationDbContext(DbContextOptions<AppDbContext> opts) : AppDbContext(opts)
+    {
+
+    }
+}
