3003bf0238f66e3ab60be6a73616aa940a05f882
Andrey O. <ozzy.ext@gmail.com>
2024-07-04 19:35:09 +0300

    Support get email case

--- src/MyLab.EmailManager.App/Features/GetEmail/EmailViewModel.cs
+++ src/MyLab.EmailManager.App/Features/GetEmail/EmailViewModel.cs
@@ -6,6 +6,6 @@ public class EmailViewModel
 {
     public required Guid Id { get; init; }
     public required string Address { get; init; }
-    public required IReadOnlyDictionary<string, string> Labels { get; init; }
+    public required IReadOnlyDictionary<string, string?> Labels { get; init; }
     public required IReadOnlyCollection<MessageViewModel> Tail { get; init; }
 }
--- src/MyLab.EmailManager.App/Features/GetEmail/GetEmailHandler.cs
+++ src/MyLab.EmailManager.App/Features/GetEmail/GetEmailHandler.cs
@@ -1,14 +1,29 @@
-﻿using MediatR;
+﻿using System.Linq.Expressions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
-using MyLab.EmailManager.Infrastructure;
+using MyLab.EmailManager.App.Common.ViewModels;
+using MyLab.EmailManager.Infrastructure.EfModels;
 
 namespace MyLab.EmailManager.App.Features.GetEmail;
 
-public class GetEmailHandler(AppDbContext dbContext) : IRequestHandler<GetEmailQuery, EmailViewModel>
+public class GetEmailHandler(ReadDbContext dbContext) : IRequestHandler<GetEmailQuery, EmailViewModel?>
 {
-    public async Task<EmailViewModel> Handle(GetEmailQuery getEmailQuery, CancellationToken cancellationToken)
+    public static readonly Expression<Func<DbEmail, EmailViewModel>> EmailConverter = dbEmail => new EmailViewModel
     {
-        throw new NotImplementedException();
-        //dbContext.Database.SqlQuery<EmailViewModel>();
+        Id = dbEmail.Id,
+        Address = dbEmail.Address,
+        Labels = dbEmail.Labels
+            .ToDictionary(l => l.Name, l => l.Value)
+            .AsReadOnly(),
+        Tail = Array.Empty<MessageViewModel>()
+    };
+
+    public Task<EmailViewModel?> Handle(GetEmailQuery getEmailQuery, CancellationToken cancellationToken)
+    {
+        return dbContext.Emails
+            .Where(e => e.Id == getEmailQuery.EmailId)
+            .Include(e => e.Labels)
+            .Select(EmailConverter)
+            .FirstOrDefaultAsync(cancellationToken: cancellationToken);
     }
 }
